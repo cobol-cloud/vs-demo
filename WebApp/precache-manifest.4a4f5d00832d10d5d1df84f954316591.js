0b402576b64475b7598d2312d3f653191a948d34
SezginH <Sezgin.Halibov@microfocus.com>
2018-11-13 12:00:42 +0200

    Initial revision

---
+++ WebApp/precache-manifest.4a4f5d00832d10d5d1df84f954316591.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "933ae3c6a842d102d079",
+    "url": "./static/js/main.933ae3c6.chunk.js"
+  },
+  {
+    "revision": "54dbe162eb8f25e14a2d",
+    "url": "./static/js/1.54dbe162.chunk.js"
+  },
+  {
+    "revision": "933ae3c6a842d102d079",
+    "url": "./static/css/main.ff700c6b.chunk.css"
+  },
+  {
+    "revision": "a2c3e5ee1738da5dfd6db20db0b66c66",
+    "url": "./index.html"
+  }
+];
